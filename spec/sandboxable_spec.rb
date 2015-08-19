705cdfea61af8c45e65fb3edced5b15775534033
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-08-19 12:07:33 +0200

    init guard

--- spec/sandboxable_spec.rb
+++ spec/sandboxable_spec.rb
@@ -4,8 +4,4 @@ describe Sandboxable do
   it 'has a version number' do
     expect(Sandboxable::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
